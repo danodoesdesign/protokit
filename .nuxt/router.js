5230f9bc9de769202877804d8ed2c9262496d03b
dano does design <dano.does.design@gmail.com>
2020-12-02 01:36:14 +1100

    Initial commit

---
+++ .nuxt/router.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _c91c9334 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/",
+    component: _c91c9334,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+  const resolve = router.resolve.bind(router)
+
+  // encodeURI(decodeURI()) ~> support both encoded and non-encoded urls
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = encodeURI(decodeURI(to))
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
